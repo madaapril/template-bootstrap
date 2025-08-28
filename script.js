ab38cc844aff42d06730be769e56fa1b0709393d
madaapril <mohammada.aprilianto@gmail.com>
2025-08-28 07:53:40 +0700

    setup awal template

---
+++ script.js
@@ -0,0 +1,32 @@
+const hamburger = $('.toggle-btn');
+const toggler = $('#icon');
+hamburger.click(function(){
+    $('#sidebar').toggleClass('expand');
+    toggler.toggleClass('ri-arrow-right-double-line');
+    toggler.toggleClass('ri-arrow-left-double-line');
+})
+
+// CHARTJS
+new Chart(document.getElementById("bar-chart-grouped"), {
+    type: 'bar',
+    data: {
+      labels: ["1900", "1950", "1999", "2050"],
+      datasets: [
+        {
+          label: "Africa",
+          backgroundColor: "#3e95cd",
+          data: [133,221,783,2478]
+        }, {
+          label: "Europe",
+          backgroundColor: "#8e5ea2",
+          data: [408,547,675,734]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Population growth (millions)'
+      }
+    }
+});
